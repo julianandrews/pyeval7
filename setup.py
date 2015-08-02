47b391a5514fe0c77ebf994d810a754f7f1a8e1b
Julian Andrews <jandrews271@gmail.com>
2015-08-01 18:30:25 -0600

    Rework setup.py to work without cython.

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
+from setuptools import setup
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    from distutils.extension import Extension
+    extensions = [Extension(name, ['eval7/%s.c' % name])
+                  for name in ('wh_rand', 'cards', 'equity', 'eval7')]
+else:
+    extensions = cythonize('eval7/*.pyx')
 
 setup(
-    name = "Eval7",
-    version = "0.1",
-    description = "Poker Evaluator and Equity Calculator",
-    packages = ['eval7'],
-    ext_modules = cythonize('eval7/*.pyx'),
-    install_requires = ['cython', 'pyparsing'],
+    name='Eval7',
+    version='0.1.1',
+    description='A poker hand evaluation and equity calculation library',
+    url='https://github.com/JulianAndrews/pyeval7',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Cython',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Games/Entertainment',
+    ],
+    keywords='poker equity library',
+    packages=['eval7'],
+    ext_modules=extensions,
+    install_requires=['pyparsing'],
 )
