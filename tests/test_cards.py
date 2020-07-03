a3e52a8224d2fa98e0bcc224f6dab7bf26d72a6e
Julian Andrews <jandrews@fusionbox.com>
2020-07-03 12:49:02 -0400

    Don't use f-strings
    I forgot these are only supported in Python 3.6+

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -11,11 +11,11 @@ class TestCards(unittest.TestCase):
     def test_suits(self):
         for rank in eval7.ranks:
             for i, suit in enumerate(eval7.suits):
-                card = eval7.Card(f'{rank}{suit}')
+                card = eval7.Card(rank + suit)
                 self.assertEqual(card.suit, i)
 
     def test_ranks(self):
         for i, rank in enumerate(eval7.ranks):
             for suit in eval7.suits:
-                card = eval7.Card(f'{rank}{suit}')
+                card = eval7.Card(rank + suit)
                 self.assertEqual(card.rank, i)
