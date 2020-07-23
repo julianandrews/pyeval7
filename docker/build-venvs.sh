aab3ebf7060924543edfb898754e7f1ce59bc7dc
Julian Andrews <jandrews@fusionbox.com>
2020-07-23 17:24:42 -0400

    Remove docker config
    The github-actions appproach is so much cleaner!

--- docker/build-venvs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for PYTHON in 'python3.7' 'python3.8'; do
-  VENV="/venv-$(basename "$PYTHON")"
-  python -m virtualenv -p "$PYTHON" "$VENV"
-  . "${VENV}/bin/activate"
-  pip install auditwheel cython
-  deactivate
-done
