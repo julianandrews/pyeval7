33fe981f1c01ac4a05ac9174e1ae4ceb659bcfc0
Julian Andrews <jandrews@fusionbox.com>
2021-01-16 09:12:05 -0500

    Hump version number for bugfix release

--- setup.py
+++ setup.py
@@ -1,34 +1,35 @@
 from os import path
-from setuptools import setup
+
 from Cython.Build import cythonize
+from setuptools import setup
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
-extensions = cythonize('eval7/*.pyx')
+extensions = cythonize("eval7/*.pyx")
 
 setup(
-    name='eval7',
-    version='0.1.8',
-    description='A poker hand evaluation and equity calculation library',
+    name="eval7",
+    version="0.1.9",
+    description="A poker hand evaluation and equity calculation library",
     long_description=long_description,
-    long_description_content_type='text/x-rst',
-    url='https://github.com/JulianAndrews/pyeval7',
-    license='MIT',
+    long_description_content_type="text/x-rst",
+    url="https://github.com/JulianAndrews/pyeval7",
+    license="MIT",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Cython',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Games/Entertainment',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Cython",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Games/Entertainment",
     ],
-    keywords='poker equity library',
-    packages=['eval7'],
+    keywords="poker equity library",
+    packages=["eval7"],
     ext_modules=extensions,
-    install_requires=['pyparsing', 'future'],
+    install_requires=["pyparsing", "future"],
 )
